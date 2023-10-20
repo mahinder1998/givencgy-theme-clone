142d6d6f46ccc219ea54b00677e0456bcafc9800
Mahinder Kumar <mahinderkumar710@gmail.com>
2023-10-21 01:17:40 +0530

    Givenchy Theme for scrtch setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/csutomers/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
