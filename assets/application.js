f4743c74f688151e9e5bcc7006f0de0fd229c22e
Mahinder Kumar <mahinderkumar710@gmail.com>
2023-10-24 23:48:58 +0530

    header layout wip

--- assets/application.js
+++ assets/application.js
@@ -1,16 +1,49 @@
 // Put your application javascript here
 
 
+let mainNav = document.querySelector(".main-navbar");
 let menuButton = document.querySelector(".menu-toggle");
 let mobileMenu = document.querySelector(".mobile-menu-wrap");
-let menuClose = document.querySelector(".menu-close");
+let menuClose = document.querySelectorAll(".menu-close");
+let subMenuBtn = document.querySelectorAll(".submenu-button");
+let submenuBack = document.querySelectorAll(".submenu-back");
+let subMenuConatiner = document.querySelectorAll(".submenu-container");
+
 
 menuButton.addEventListener("click", function(){
     mobileMenu.classList.add("mobile-menu-active");
-    this.parentNode.parentNode.parentNode.parentNode.classList.add("menu-active")
+    mainNav.classList.add("menu-active");
 })
 
-menuClose.addEventListener("click", function(){
-    mobileMenu.classList.remove("mobile-menu-active");
-    this.parentNode.parentNode.parentNode.parentNode.parentNode.classList.remove("menu-active")
+//
+function closeMenu(){
+    subMenuConatiner.forEach(function(e){
+        e.classList.remove("submenu-open");
+    })
+}
+
+menuClose.forEach(function(closebtn){
+    closebtn.addEventListener("click", function(){
+        mobileMenu.classList.remove("mobile-menu-active");
+        mainNav.classList.remove("menu-active");
+        closeMenu();
+    })
+})
+
+
+
+// submenu btn on click
+subMenuBtn.forEach(function(submenubtn){
+    submenubtn.addEventListener("click", function(){
+        const submenuNext = submenubtn.nextElementSibling;
+        submenuNext.classList.add("submenu-open")
+    })
+})
+
+// submenu back
+submenuBack.forEach(function(submenuback){ 
+    submenuback.addEventListener("click", function(){
+        const backstep = submenuback.parentNode.parentNode;
+        backstep.classList.remove("submenu-open");
+    })
 })
