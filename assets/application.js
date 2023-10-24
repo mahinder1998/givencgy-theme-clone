7f79311cac0b4cf8c5d6501d66ad94381b4944d1
Mahinder Kumar <mahinderkumar710@gmail.com>
2023-10-24 15:55:46 +0530

    header layout inprogress

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,16 @@
 // Put your application javascript here
+
+
+let menuButton = document.querySelector(".menu-toggle");
+let mobileMenu = document.querySelector(".mobile-menu-wrap");
+let menuClose = document.querySelector(".menu-close");
+
+menuButton.addEventListener("click", function(){
+    mobileMenu.classList.add("mobile-menu-active");
+    this.parentNode.parentNode.parentNode.parentNode.classList.add("menu-active")
+})
+
+menuClose.addEventListener("click", function(){
+    mobileMenu.classList.remove("mobile-menu-active");
+    this.parentNode.parentNode.parentNode.parentNode.parentNode.classList.remove("menu-active")
+})
